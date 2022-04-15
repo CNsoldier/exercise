f5ce88190ca055d9a1fd24ec0ca0c9ccc8b1abf2
burgess <burgess.wang@kapeixi.com>
2022-04-15 16:44:36 +0800

    初始化

---
+++ camunda/src/main/java/student.java
@@ -0,0 +1,14 @@
+import lombok.Data;
+
+/**
+ * @program: exercise
+ * @description:
+ * @author: burgess
+ * @create: 2022-04-15 16:43
+ **/
+@Data
+public class student {
+
+    private String name;
+    private String age;
+}
